15f881d0dac97583aa7ac1e556d53838597d23ca
igoryan <nkulinigor1984@icloud.com>
2022-05-24 21:28:56 +0200

    task_02

---
+++ task_02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] GetTableDouble(int m, int n)
+{
+    double[,] table = new double[m, n];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return table;
+}
+
+void PrintTable(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            Console.Write(tab[i, j] + "    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 8;
+int n = 4;
+double[,] TableDouble = GetTableDouble(m, n);
+PrintTable(TableDouble);
