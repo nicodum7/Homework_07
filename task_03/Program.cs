8b6332cae0658896d15b9d2d255e6c80dbbe985b
igoryan <nkulinigor1984@icloud.com>
2022-05-24 21:40:52 +0200

    task_03

---
+++ task_03/Program.cs
@@ -0,0 +1,49 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+int[,] GetTable(int row, int col, int min, int max)
+{
+    int[,] firstTab = new int[row, col];
+    for (int i = 0; i < firstTab.GetLength(0); i++)
+    {
+        for (int j = 0; j < firstTab.GetLength(1); j++)
+        {
+            firstTab[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return firstTab;
+}
+
+int[,] ReplaceEvenToSquare(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0) array[i, j] = array[i, j] * (-1);
+        }
+    }
+    return array;
+}
+
+void PrintTable(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 10;
+int min = 1;
+int max = 10;
+
+int[,] firstArray = GetTable(row, col, min, max);
+PrintTable(firstArray);
+Console.WriteLine("*******************************");
+int[,] replaceArray = ReplaceEvenToSquare(firstArray);
+PrintTable(replaceArray);
